976320333cb8f7e84795c4e5b28c7fd828829bbe
Evelyn dos Santos <evelynsantos@ibm.com>
2022-07-27 11:54:48 -0300

    :sparkles: Criando operação de isntanciação :sparkles:

--- src/main/java/com/spring/demo/DemoApplication.java
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -1,13 +1,38 @@
 package com.spring.demo;
 
+
+
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.spring.demo.domain.Categoria;
+import com.spring.demo.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class DemoApplication {
+// Permitir implementar um metodo auxiliar quando a aplicação iniciar
+public class DemoApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));;
+		
 	}
 
 }
